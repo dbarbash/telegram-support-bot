b440cca488c28c8c66c16df6bafcf13d72477263
dbarbash <78915298+dbarbash@users.noreply.github.com>
2022-05-27 18:48:10 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -17,6 +17,7 @@ if TELEGRAM_SUPPORT_CHAT_ID is None or not str(TELEGRAM_SUPPORT_CHAT_ID).lstrip(
 TELEGRAM_SUPPORT_CHAT_ID = int(TELEGRAM_SUPPORT_CHAT_ID)
 
 
-WELCOME_MESSAGE = os.getenv("WELCOME_MESSAGE", "👋")
+WELCOME_MESSAGE = os.getenv("WELCOME_MESSAGE", "Здравствуйте! 
+Мы ответим Вам в ближайшее время")
 REPLY_TO_THIS_MESSAGE = os.getenv("REPLY_TO_THIS_MESSAGE", "REPLY_TO_THIS")
 WRONG_REPLY = os.getenv("WRONG_REPLY", "WRONG_REPLY")
